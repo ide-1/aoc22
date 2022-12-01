8ea7312f13bbb01ba8d21ed505edf03655d0d955
ide-1 <ida.josephine.dahl@gmail.com>
2022-12-01 06:33:10 +0100

    day 1

---
+++ day1/main.py
@@ -0,0 +1,42 @@
+
+with open("input.txt") as file:
+    input = file.read()
+    input = input.splitlines()
+#print(input)
+max = 0
+elf = 0
+for i in input :
+    
+    if i == "":
+        if elf > max:
+            max = elf
+        elf = 0
+
+    else:
+        elf += int(i)
+
+print(max)
+
+one = 0
+two = 0
+three = 0
+
+for i in input :
+    
+    if i == "":
+        if elf> one: 
+            three = two
+            two = one
+            one = elf
+        elif elf > two:
+            three = two
+            two = elf
+        elif elf > three:
+            three = elf
+        
+        elf = 0
+
+    else:
+        elf += int(i)
+
+print (one+two+three)
